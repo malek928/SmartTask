ff0eee99c4133d86b69086f338f61b2c6ae8230c
maalleekk <maalekmarouanii@gmail.com>
2026-04-20 00:12:15 +0100

    dbdone

--- src/main/java/com/smarttask/utils/DatabaseConnection.java
+++ src/main/java/com/smarttask/utils/DatabaseConnection.java
@@ -1,16 +1,38 @@
 package com.smarttask.utils;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/smarttask_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "12345678";
-
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        Properties props = new Properties();
+
+        // 1. Look inside the "resources" folder for the secret file
+        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
+
+            if (input == null) {
+                System.out.println("ERROR: Cannot find db.properties. Please create it in the resources folder!");
+                return null;
+            }
+
+            // 2. Load the file
+            props.load(input);
+
+            // 3. Read the values from the file
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            // 4. Connect to MySQL!
+            return DriverManager.getConnection(url, user, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new SQLException("Failed to connect to the database.");
+        }
     }
 }
