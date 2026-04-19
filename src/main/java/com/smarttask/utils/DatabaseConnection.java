9457640a4430d9c97a527858483ba16252fc2423
maalleekk <maalekmarouanii@gmail.com>
2026-04-20 00:44:19 +0100

    update

--- src/main/java/com/smarttask/utils/DatabaseConnection.java
+++ src/main/java/com/smarttask/utils/DatabaseConnection.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+
 public class DatabaseConnection {
 
     public static Connection getConnection() throws SQLException {
